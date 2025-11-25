cfc7562815cee733320fcb2e8e64b20b988c3308
Kae-72 <ouverdraiva@gmail.com>
2025-11-25 12:28:15 -0500

    trying to implement a buffer but trial and error

--- Thereorchestra/Thereorchestra.ino
+++ Thereorchestra/Thereorchestra.ino
@@ -11,76 +11,64 @@ int NOTE_C5 = 523;
 //NOTES
 
 
-const int N = 400;
-int buffer[N];
-int indexBuffer = 0;
-long addup = 0;
-bool full = false;
+// const int N = 100;
+// int buffer[N];
+// int indexBuffer = 0;
+// long addup = 0;
+// bool full = false;
 
+int OCTAVER = 0;
 int buzzer = 10;
 
 void setup() {
 
   pinMode(buzzer, OUTPUT);
   pinMode(A0, INPUT);
-  pinMode(A1, INPUT)
-  Serial.begin(9600);
+  pinMode(A1, INPUT);
+  Serial.begin(115200);
 
 }
 
 void loop() {
 
+  // if (OCTAVER = 0) {
+  //   pinMode(buzzer, INPUT);
+  // }
+  // else{
+  //   pinMode(buzzer, OUTPUT);
+  // }
+
   //SENSOR 1 (pitch changer)
   int sensor1valueAVG = analogRead(A0);
 
-  // if (full) {
-  //   addup -= buffer[indexBuffer];
-  // }
-
-  // buffer[indexBuffer] = sensor1valueAVG;
-
-  // addup += sensor1valueAVG;
-
-  // indexBuffer = (indexBuffer + 1) % N;
-
-  // if (indexBuffer == 0) {
-  //   full = true;
-  // }
-
-  // if (full) {
-  //   float promedio = addup / (float)N;
-
-
   Serial.print("sensor1value average: ");
   Serial.println(sensor1valueAVG);
-  //}
+  
   //SENSOR 1 (pitch changer)
 
   //SENSOR 2 (octave changer)
-  int sensor2value = analogRead(A1)
+  int sensor2value = analogRead(A1);
 
-  Serial.print("sensor2value average: ");
-  Serial.printIn(sensor2value);
+  Serial.print("sensor2value: ");
+  Serial.println(sensor2value);
   //SENSOR 2 (octave changer)
 
-  delay(200);
 
 //OCTAVE CHANGER----------------------------------------------------------------------------------------------------------------------------------//
   if (sensor2value >= 500) {
-    OCTAVER = 1
+    int OCTAVER = 1;
     pinMode(buzzer, OUTPUT);
   }
   else if (sensor2value >= 300) {
-    OCTAVER = 2
+    int OCTAVER = 2;
     pinMode(buzzer, OUTPUT);
   }
   else if (sensor2value >= 100) {
-    OCTAVER = 4
+    int OCTAVER = 4;
     pinMode(buzzer, OUTPUT);
   }
   else {
-    OCTAVER = 0
-    pinMode(buzzer, INPUT);
+    int OCTAVER = 0;
   } 
 // possibly rearrange the values needed
 
@@ -107,7 +95,7 @@ void loop() {
     int CURRENT_NOTE = NOTE_B4 * OCTAVER;
     tone(buzzer, CURRENT_NOTE);
   } else {
-    noTone(buzzer)
+    noTone(buzzer);
   }
 
   
